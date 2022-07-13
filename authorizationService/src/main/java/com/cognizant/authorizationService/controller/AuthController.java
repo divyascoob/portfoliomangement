12abce670de1a0930cb17ce5ee19fa837746c179
DIVYA <DIVYA@192.168.0.107>
2022-07-13 22:17:24 +0530

    edit

--- authorizationService/src/main/java/com/cognizant/authorizationService/controller/AuthController.java
+++ authorizationService/src/main/java/com/cognizant/authorizationService/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import com.cognizant.authorizationService.service.JwtUtil;
  * getting the token and validating the token this class will be used.
  */
 @RestController
+@CrossOrigin
 public class AuthController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
